6541440cfa6cacfad28be0b073c4cf8e3fe30a41
DylanG <dylanga98backup@gmail.com>
2011-02-28 19:46:41 -0500

    Registered _LOGIN _JOIN and _KICK. Some typo fixes

--- src/AllGamer/AGBS/AGBS.java
+++ src/AllGamer/AGBS/AGBS.java
@@ -1,19 +1,17 @@
 package AllGamer.AGBS;
 
 import java.io.File;
-
 import java.util.HashMap;
-import org.bukkit.entity.Player;
+
 import org.bukkit.Server;
-import org.bukkit.event.Event.Priority;
+import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
+import org.bukkit.event.Event.Priority;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.PluginLoader;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.plugin.PluginManager;
-
-import sun.rmi.runtime.Log;
 
 // permissions 2.4 imports
 import com.nijiko.permissions.PermissionHandler;
@@ -25,60 +23,63 @@ import com.nijikokun.bukkit.Permissions.Permissions;
  * @author AllGamer
  */
 public class AGBS extends JavaPlugin {
-    private final AGBSPlayerListener playerListener = new AGBSPlayerListener(this);
-    private final AGBSBlockListener blockListener = new AGBSBlockListener(this);
-    private final HashMap<Player, Boolean> debugees = new HashMap<Player, Boolean>();
-    
-    // use permissions 2.4 or >
-    public static PermissionHandler Permissions = null;
-    
-    // check to see if we have permissions, or fake permissions. Disable if we don't...
-    public void setupPermissions() {
-    	Plugin agbs = this.getServer().getPluginManager().getPlugin("Permissions");
-    	PluginDescriptionFile pdfFile = this.getDescription();
-    		
-    	if (this.Permissions == null) {
-    		if (agbs!= null) {
-    			this.getServer().getPluginManager().enablePlugin(agbs);
-    			this.Permissions = ((Permissions) agbs).getHandler();
-    		}
-    		else {
-    			System.out.println(pdfFile.getName() + " version " + pdfFile.getVersion() + "not enabled. Permissions not detected");
-    			this.getServer().getPluginManager().disablePlugin(this);
-    		}
-    	}
-    }
-    
-    public void onEnable() {
-        // TODO: Place any custom enable code here including the registration of any events
+	private final AGBSPlayerListener playerListener = new AGBSPlayerListener(this);
+	private final AGBSBlockListener blockListener = new AGBSBlockListener(this);
+	private final HashMap<Player, Boolean> debugees = new HashMap<Player, Boolean>();
 
-    	setupPermissions();
-    	
-        // Register our events
-        PluginManager pm = getServer().getPluginManager();
-        {
-        }
-    }
+	// use permissions 2.4+
+	public static PermissionHandler Permissions = null;
 
-    public void onDisable() {
-        // TODO: Place any custom disable code here
 
-        // NOTE: All registered events are automatically unregistered when a plugin is disabled
+	// check to see if we have permissions, or fake permissions. Disable if we don't...
+	public void setupPermissions() {
+		Plugin agbs = this.getServer().getPluginManager().getPlugin("Permissions");
+		PluginDescriptionFile pdfFile = this.getDescription();
 
-        // EXAMPLE: Custom code, here we just output some info so we can check all is well
-        PluginDescriptionFile pdfFile = this.getDescription();
-    	System.out.println( pdfFile.getName() + " version " + pdfFile.getVersion() + "Disabled!" );
-    }
-    public boolean isDebugging(final Player player) {
-        if (debugees.containsKey(player)) {
-            return debugees.get(player);
-        } else {
-            return false;
-        }
-    }
+		if (this.Permissions == null) {
+			if (agbs!= null) {
+				this.getServer().getPluginManager().enablePlugin(agbs);
+				this.Permissions = ((Permissions) agbs).getHandler();
+			}
+			else {
+				System.out.println(pdfFile.getName() + " version " + pdfFile.getVersion() + " not enabled. Permissions not detected");
+				this.getServer().getPluginManager().disablePlugin(this);
+			}
+		}
+	}
 
-    public void setDebugging(final Player player, final boolean value) {
-        debugees.put(player, value);
-    }
+	public void onEnable() {
+		// TODO: Place any custom enable code here including the registration of any events
+
+		setupPermissions();
+
+		// Register our events
+		PluginManager pm = getServer().getPluginManager();
+		{
+			getServer().getPluginManager().registerEvent(Event.Type.PLAYER_LOGIN, playerListener, Priority.Normal, this);
+			getServer().getPluginManager().registerEvent(Event.Type.PLAYER_JOIN, playerListener, Priority.Normal, this);
+			getServer().getPluginManager().registerEvent(Event.Type.PLAYER_KICK, playerListener, Priority.Normal, this);
+		}
+	}
+
+	public void onDisable() {
+		// TODO: Place any custom disable code here
+
+		// NOTE: All registered events are automatically unregistered when a plugin is disabled
+
+		// EXAMPLE: Custom code, here we just output some info so we can check all is well
+		PluginDescriptionFile pdfFile = this.getDescription();
+		System.out.println( pdfFile.getName() + " version " + pdfFile.getVersion() + " disabled!" );
+	}
+	public boolean isDebugging(final Player player) {
+		if (debugees.containsKey(player)) 
+			return debugees.get(player);
+
+		return false;
+	}
+
+	public void setDebugging(final Player player, final boolean value) {
+		debugees.put(player, value);
+	}
 }
 
