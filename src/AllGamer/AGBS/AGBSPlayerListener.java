018e2ffe29492708c51a2114122d4622d1c983e3
DylanG <dylanga98backup@gmail.com>
2011-02-28 21:24:29 -0500

    Kicks the user by player with a reason. Need to write to db/text to reject login (ban)

--- src/AllGamer/AGBS/AGBSPlayerListener.java
+++ src/AllGamer/AGBS/AGBSPlayerListener.java
@@ -11,11 +11,23 @@ import org.bukkit.event.player.PlayerLoginEvent;
  */
 public class AGBSPlayerListener extends PlayerListener {
 	private final AGBS plugin;
+	String message;
 
 	public AGBSPlayerListener(AGBS instance) {
 		plugin = instance;
 	}
 
+	public String make(String[] split, int startingIndex) {
+		message = "";
+		for (; startingIndex < split.length; startingIndex++) {
+			if (startingIndex == 1)
+				message += "" + split[startingIndex];
+			else
+				message += " " + split[startingIndex];
+		}
+		return message;
+	}
+	
 	public boolean arraySearch(Player[] list, Player target) {
 		for (Player p : list)
 			if (p.equals(target)) {
@@ -39,19 +51,20 @@ public class AGBSPlayerListener extends PlayerListener {
 		if(sender instanceof Player) {
 			player = (Player) sender;
 		}
-		// /a ban [target]
+		// /a ban [target] [reason]
 		if (commandLabel.equals("a")) {
 			if (split[0].equalsIgnoreCase("ban")) {
 				if (AGBS.Permissions.has(player, "agbs.ban") || AGBS.Permissions.has(player, "agbs.*")) {
-					if (split.length == 2) {
+					if (split.length >= 2) {
 						Player target = plugin.getServer().getPlayer(split[1]);
 						if (arraySearch(onlinePlayers, target)) {
-
+							make(split, 2);
+							target.kickPlayer("Kicked by " + player + ". Reason: " + message);
 						} else {
 							player.sendMessage("Cannot find the specified player! Check your spelling again.");
 						}
 					} else {
-						player.sendMessage("Correct usage is /a ban [target]");
+						player.sendMessage("Correct usage is /a ban [target] [reason]");
 					}
 				} else {
 					player.sendMessage("You don't have access to this command.");
