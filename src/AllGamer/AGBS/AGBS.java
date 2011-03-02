27677ee1c5e61a7749b09b88b5c80421146f1dc8
aetaric <aetaric@gmail.com>
2011-03-02 00:36:40 -0500

    added command check(disfunct until api works). fixed code to follow ONE standard. { and } should be on their own lines now.

--- src/AllGamer/AGBS/AGBS.java
+++ src/AllGamer/AGBS/AGBS.java
@@ -21,35 +21,40 @@ import com.nijikokun.bukkit.Permissions.Permissions;
  *
  * @author AllGamer
  */
-public class AGBS extends JavaPlugin {
+public class AGBS extends JavaPlugin 
+{
 	private final AGBSPlayerListener playerListener = new AGBSPlayerListener(this);
 	//private final AGBSBlockListener blockListener = new AGBSBlockListener(this);
 	private final HashMap<Player, Boolean> debugees = new HashMap<Player, Boolean>();
 	String message = "";
 	String reason = "";
 
-	// use permissions 2.4+
+	// use permissions 2.5+
 	public static PermissionHandler Permissions = null;
 
 
 	// check to see if we have permissions, or fake permissions. Disable if we don't...
-	public void setupPermissions() {
+	public void setupPermissions() 
+	{
 		Plugin agbs = this.getServer().getPluginManager().getPlugin("Permissions");
 		PluginDescriptionFile pdfFile = this.getDescription();
 
-		if (AGBS.Permissions == null) {
+		if (AGBS.Permissions == null) 
+		{
 			if (agbs != null) {
 				this.getServer().getPluginManager().enablePlugin(agbs);
 				AGBS.Permissions = ((Permissions) agbs).getHandler();
 			}
-			else {
+			else 
+			{
 				System.out.println(pdfFile.getName() + " version " + pdfFile.getVersion() + " not enabled. Permissions not detected");
 				this.getServer().getPluginManager().disablePlugin(this);
 			}
 		}
 	}
 
-	public void onEnable() {
+	public void onEnable() 
+	{
 		// TODO: Place any custom enable code here including the registration of any events
 		setupPermissions();
 		registerListeners();
@@ -57,7 +62,8 @@ public class AGBS extends JavaPlugin {
 		System.out.println( pdfFile.getName() + " version " + pdfFile.getVersion() + " enabled!" );
 	}
 
-	public void onDisable() {
+	public void onDisable() 
+	{
 		// TODO: Place any custom disable code here
 
 		// NOTE: All registered events are automatically unregistered when a plugin is disabled
@@ -67,23 +73,28 @@ public class AGBS extends JavaPlugin {
 		System.out.println( pdfFile.getName() + " version " + pdfFile.getVersion() + " disabled!" );
 	}
 
-	public boolean isDebugging(final Player player) {
+	public boolean isDebugging(final Player player) 
+	{
 		if (debugees.containsKey(player)) 
 			return debugees.get(player);
 
 		return false;
 	}
 
-	public void setDebugging(final Player player, final boolean value) {
+	public void setDebugging(final Player player, final boolean value) 
+	{
 		debugees.put(player, value);
 	}
-	public void registerListeners() {
+	public void registerListeners() 
+	{
 		getServer().getPluginManager().registerEvent(Event.Type.PLAYER_LOGIN, playerListener, Priority.Normal, this);
 	}
 	
-	public String make(String[] split, int startingIndex) {
+	public String make(String[] split, int startingIndex) 
+	{
 		message = "";
-		for (; startingIndex < split.length; startingIndex++) {
+		for (; startingIndex < split.length; startingIndex++) 
+		{
 			if (startingIndex == 1)
 				message += "" + split[startingIndex];
 			else
@@ -92,48 +103,62 @@ public class AGBS extends JavaPlugin {
 		return message;
 	}
 
-	public boolean arraySearch(Player[] list, Player target) {
+	public boolean arraySearch(Player[] list, Player target) 
+	{
 		for (Player p : list)
-			if (p.equals(target)) {
+			if (p.equals(target)) 
+			{
 				return true;
 			}
 		return false;
 	}
 
-	public String makeReason(String message) {
-		if (message.contains("grf")) {
+	public String makeReason(String message) 
+	{
+		if (message.contains("grf")) 
+		{
 			reason += " Griefing";
 		}
-		if (message.contains("hax")) {
+		if (message.contains("hax")) 
+		{
 			reason += " Hacking";
 		}
-		if (message.contains("thf")) {
+		if (message.contains("thf")) 
+		{
 			reason += " Theft";
 		}
-		if (message.contains("dis")) {
+		if (message.contains("dis")) 
+		{
 			reason += " Discriminatory Comments";
 		}
-		if (message.contains("lan")) {
+		if (message.contains("lan")) 
+		{
 			reason += " Foul Language (Cursing)";
 		}
-		if (message.contains("bld")) {
+		if (message.contains("bld")) 
+		{
 			reason += " Inappropriate Buildings";
 		}
 		return reason;
 	}
 	
-	public boolean onCommand(CommandSender sender, Command commandArg, String commandLabel, String[] args) {
+	public boolean onCommand(CommandSender sender, Command commandArg, String commandLabel, String[] args) 
+	{
 		Player player = (Player) sender;
 		Server server = getServer();
 		String command = commandArg.getName().toLowerCase();
 		String[] split = args;
 		Player[] onlinePlayers = getServer().getOnlinePlayers();
 		
-		if (command.equalsIgnoreCase("aban")) {
-			if (AGBS.Permissions.has(player, "agbs.ban") || AGBS.Permissions.has(player, "agbs.*") ||  AGBS.Permissions.has(player, "*")) {
-				if (split.length >= 2) {
+		if (command.equalsIgnoreCase("aban")) 
+		{
+			if (AGBS.Permissions.has(player, "agbs.ban") || AGBS.Permissions.has(player, "agbs.*") ||  AGBS.Permissions.has(player, "*")) 
+			{
+				if (split.length >= 2) 
+				{
 					Player target = getServer().getPlayer(split[0]);
-					if (arraySearch(onlinePlayers, target)) {
+					if (arraySearch(onlinePlayers, target)) 
+					{
 						message = make(split, 1);
 						message = message.toLowerCase();
 						reason = makeReason(message);
@@ -147,22 +172,32 @@ public class AGBS extends JavaPlugin {
 						// TODO: code for sending ban info to the api
 						// TODO: Discuss: should this be done once to clean up code maybe with the syntax banPlayer( target, reason, apikey); ? 
 
-					} else {
+					} 
+					else 
+					{
 						player.sendMessage("Cannot find the specified player! Check your spelling again.");
 					}
-				} else {
+				} 
+				else 
+				{
 					player.sendMessage("Correct usage is /aban [target] [reason]");
 				}
-			} else {
+			} 
+			else 
+			{
 				player.sendMessage("You don't have access to this command.");
 			}
 			return true;
 		}
-		if (commandLabel.equalsIgnoreCase("abanip")) {
-				if (AGBS.Permissions.has(player, "agbs.banip") || AGBS.Permissions.has(player, "agbs.*") || AGBS.Permissions.has(player, "*")) {
-					if (split.length >= 2) {
+		if (commandLabel.equalsIgnoreCase("abanip")) 
+		{
+				if (AGBS.Permissions.has(player, "agbs.banip") || AGBS.Permissions.has(player, "agbs.*") || AGBS.Permissions.has(player, "*")) 
+				{
+					if (split.length >= 2) 
+					{
 						Player target = getServer().getPlayer(split[0]);	
-						if (arraySearch(onlinePlayers, target)) {
+						if (arraySearch(onlinePlayers, target)) 
+						{
 							message = make(split, 1);
 							message = message.toLowerCase();
 							reason = makeReason(message);
@@ -176,18 +211,52 @@ public class AGBS extends JavaPlugin {
 							// TODO: code for sending ban info to the api
 							// Discuss: should this be done once to clean up code or every time we call it? 
 
-						} else {
+						} 
+						else 
+						{
 							player.sendMessage("Cannot find the specified player! Check your spelling again.");
 						}
-					} else {
+					} 
+					else 
+					{
 						player.sendMessage("Correct usage is /abanip [target] [reason]");
 					}
-				} else {
+				} 
+				else 
+				{
 					player.sendMessage("You don't have access to this command.");
 				}
+				return true;
+		}
+		if (commandLabel.equalsIgnoreCase("check"))
+		{
+			if (AGBS.Permissions.has(player, "agbs.check") || AGBS.Permissions.has(player, "agbs.*") || AGBS.Permissions.has(player, "*"))
+			{
+				if (split.length >= 2) 
+				{
+					Player target = getServer().getPlayer(split[0]);	
+					if (arraySearch(onlinePlayers, target)) 
+					{
+					// TODO: code for checking target name against api here
+					
+
+					}
+					else 
+					{
+						player.sendMessage("Cannot find the specified player! Check your spelling again.");
+					}
+				} 
+				else 
+				{
+					player.sendMessage("Correct usage is /check [target]");
+				}
+			} 
+			else
+			{
+				player.sendMessage("You don't have access to this command.");
+			}
 			return true;
 		}
 		return true;
 	}
 }
-
