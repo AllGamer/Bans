7df2d7e9e2710ef5f0c09d49a12c9478710e78da
DylanG <dylanga98backup@gmail.com>
2011-03-02 21:28:43 -0500

    Rejects login if player is banzored

--- src/net/AllGamer/AGBS/AGBSPlayerListener.java
+++ src/net/AllGamer/AGBS/AGBSPlayerListener.java
@@ -2,7 +2,13 @@ package net.AllGamer.AGBS;
 
 
 import java.util.logging.Logger;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.Event;
+import org.bukkit.event.player.PlayerEvent;
 import org.bukkit.event.player.PlayerListener;
+import org.bukkit.event.player.PlayerLoginEvent;
+import org.bukkit.util.config.Configuration;
 
 /*
  * Handle events for all Player related events
@@ -13,15 +19,26 @@ public class AGBSPlayerListener extends PlayerListener
 	@SuppressWarnings("unused")
 	private final AGBS plugin;
 	private final Logger log = Logger.getLogger("Minecraft");
-	
+
 
 	public AGBSPlayerListener(AGBS instance) 
 	{
 		plugin = instance;
 	}
-	
-	
+
+
 	//ONLY ADD LOGIN STUFF HERE
+	public void onPlayerLogin(PlayerLoginEvent event)
+	{
+		Player player = event.getPlayer();
+		AGBS.configBan.load();
+		String[] x = AGBS.configBan.getString("banned").split(",");
+		String bannedPlayers = AGBS.make(x, 0);
+		if (bannedPlayers.contains(player.getDisplayName().toLowerCase()))
+		{
+			event.disallow(PlayerLoginEvent.Result.KICK_FULL, "You are banned from this server!");
+		}
+	}
 
 }
 
